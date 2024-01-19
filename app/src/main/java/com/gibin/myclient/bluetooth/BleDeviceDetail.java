056530d8c044f9acbe884f4edc5a14d2b2572926
Gibin <gibin.indocosmo@gmail.com>
2024-01-19 14:43:03 +0530

    Added BLE Service discobery

---
+++ app/src/main/java/com/gibin/myclient/bluetooth/BleDeviceDetail.java
@@ -0,0 +1,16 @@
+package com.gibin.myclient.bluetooth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.gibin.myclient.R;
+
+public class BleDeviceDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ble_device_detail);
+    }
+}
